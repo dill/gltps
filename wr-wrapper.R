34cfe850429bf69ae929e8eb2909f100304183fc
David Lawrence Miller <dave@ninepointeightone.net>
2012-11-06 10:14:08 -0500

    needed ::: to get create_distance_matrix()

--- wr-wrapper.R
+++ wr-wrapper.R
@@ -5,7 +5,7 @@ wr<-function(samp,knots,bnd,lambda=NULL,family="normal"){
 
    # expect samp to be a list with elements x,y,z 
    # where z is response
-   
+
    # need to find the distance matrix
    D<-create_distance_matrix(c(samp$x,knots$x),
                              c(samp$y,knots$y),
@@ -18,7 +18,7 @@ wr<-function(samp,knots,bnd,lambda=NULL,family="normal"){
    D.xxk<-D[1:nsamp,(nsamp+1):dim(D)[2]]
    # distances between knots
    D.xkxk<-D[(nsamp+1):dim(D)[2],(nsamp+1):dim(D)[2]]
-   
+
    # find the beta
    beta<-fit.tps(samp$z,matrix(c(samp$x,samp$y),nsamp,2),
                   matrix(c(knots$x,knots$y),length(knots$x),2),
@@ -43,9 +43,9 @@ wr.pred<-function(pred,knots,beta,bnd){
    npred<-length(pred$x)
 
    # for the prediction points
-   D.p<-create_distance_matrix(c(pred$x,knots$x),
-                               c(pred$y,knots$y),bnd,
-                               start=length(pred$x))
+   D.p <- msg:::create_distance_matrix(c(pred$x,knots$x),
+                                       c(pred$y,knots$y),bnd,
+                                       start=length(pred$x))
 
    # distances from prediction points to knots
    #D.xpxk<-D.p[1:npred,(npred+1):dim(D.p)[2]]
